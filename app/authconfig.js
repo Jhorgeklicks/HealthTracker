8f531bcaf43dc03e98c3c4f24251212eb24d271d
jhorgeklicks <acquayegeorge9@gmail.com>
2024-05-21 15:25:11 -0700

    updated config

--- app/authconfig.js
+++ app/authconfig.js
@@ -1,36 +1,18 @@
-import { redirect } from "next/navigation";
-
 export const authConfig = {
   providers:[],
   pages: {
     signIn: "/login",
   },
   callbacks: {
-     authorized({ auth, request }) {
-
+    authorized({ auth, request }) {
       const isLoggedIn = auth?.user;
       const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
-
-      // old
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-
-        return (`${request.nextUrl.href}/dashboard`);
-
-        // console.log(request.nextUrl.href)
-        // return Response.redirect(new URL("/dashboard", request.nextUrl));
-        // return Response.redirect(new URL("/dashboard", request.nextUrl));
+        return Response.redirect(new URL("/dashboard", request.nextUrl));
       }
-
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) return true;
-      //   return false;
-      // } else if (isLoggedIn) {
-      //   return "/dashboard"; 
-      // }
-    
       return true;
     },
   },
