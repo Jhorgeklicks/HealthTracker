e2f94676142b50c537c63b933b7119a5589c601d
jhorgeklicks <acquayegeorge9@gmail.com>
2024-05-21 08:41:34 -0700

    updated authConfig

--- app/authconfig.js
+++ app/authconfig.js
@@ -7,12 +7,22 @@ export const authConfig = {
     authorized({ auth, request }) {
       const isLoggedIn = auth?.user;
       const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+      // old
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false;
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL("/dashboard", request.nextUrl));
+      // }
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", request.nextUrl));
+        const dashboardUrl = new URL("/dashboard", request.nextUrl);
+        return Response.redirect(dashboardUrl);
       }
+    
       return true;
     },
   },
